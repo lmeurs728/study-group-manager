7561a852655c3274e767485b2f58e4ad4d60c891
Lance Meurs <lmeurs728@gmail.com>
2021-04-06 16:40:58 -0600

    getPeople

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,27 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import moment from "moment"
-import sampleData from "./sampleData.json"
+// import moment from "moment"
+// import sampleData from "./sampleData.json"
 
 Vue.config.productionTip = false
 
-function injectAvailabilityToBots() {
-	sampleData.forEach((bot, i, bots) => {
-		bots[i].availability = {};
-		for (var day = 0; day < 7; day++) {
-			var dailyAvailability = [];
-			for (var hour = 0; hour < 13; hour++) {
-				dailyAvailability.push(Math.floor(Math.random() *  3));
-			}
-			bots[i].availability[moment.weekdays(day)] = dailyAvailability;
-		}
-	})
+// function injectAvailabilityToBots() {
+// 	sampleData.forEach((bot, i, bots) => {
+// 		bots[i].availability = {};
+// 		for (var day = 0; day < 7; day++) {
+// 			var dailyAvailability = [];
+// 			for (var hour = 0; hour < 13; hour++) {
+// 				dailyAvailability.push(Math.floor(Math.random() *  3));
+// 			}
+// 			bots[i].availability[moment.weekdays(day)] = dailyAvailability;
+// 		}
+// 	})
 
-	return sampleData;
-}
+// 	return sampleData;
+// }
   
-const people = injectAvailabilityToBots();
+// const people = injectAvailabilityToBots();
+
+async function getPeople() {
+
+}
+const people = getPeople()
 
 new Vue({
 router,
